750b56ee81274b5b002a679f8eff2b2ac7a120ae
Tamires Santana da Silva <katarina_luiza@icloud.com>
2025-08-10 14:48:17 -0300

    Adicionando testes de transferencia

---
+++ test/transferencias.test.js
@@ -0,0 +1,54 @@
+const request = require('supertest'); // <-- Corrigido aqui
+const { expect } = require('chai');
+
+describe('Transferencias', () => {
+  describe('POST /transferencias', () => {
+    it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de R$ 10,00', async () => {
+      const respostaLogin = await request('http://localhost:3000')
+        .post('/login')
+        .set('Content-Type', 'application/json')
+        .send({
+          username: 'julio.lima',
+          senha: '123456'
+        });
+
+      const token = respostaLogin.body.token;
+
+      const resposta = await request('http://localhost:3000')
+        .post('/transferencias')
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+          contaOrigem: 1,
+          contaDestino: 2,
+          valor: 11
+        });
+
+      expect(resposta.status).to.equal(201);
+    });
+
+    it('Deve retornar falha com 422 quando o valor da transferência for abaixo de R$10,00', async () => {
+      const respostaLogin = await request('http://localhost:3000')
+        .post('/login')
+        .set('Content-Type', 'application/json')
+        .send({
+          username: 'julio.lima',
+          senha: '123456'
+        });
+
+      const token = respostaLogin.body.token;
+
+      const resposta = await request('http://localhost:3000')
+        .post('/transferencias')
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+          contaOrigem: 1,
+          contaDestino: 2,
+          valor: 7
+        });
+
+      expect(resposta.status).to.equal(422);
+    });
+  });
+});
